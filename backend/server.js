ff97b567f6fc6a01e4bf756a03e86480d3827971
Prateek Labroo <50594243+PLabroo@users.noreply.github.com>
2024-10-05 02:08:44 +0530

    added formik and error handling to contact form

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use('/contactForm', contactRoutes); // Main route for contact form
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port:${PORT}`);
 });
--- frontend/src/components/contactForm.tsx
+++ frontend/src/components/contactForm.tsx
@@ -1,90 +1,31 @@
-import {
-  Box,
-  Button,
-  TextField,
-  Theme,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { useState } from "react";
-import emailjs from "@emailjs/browser";
-import { useSnackbar } from "notistack";
+import { Box, TextField, Typography, useMediaQuery } from '@mui/material';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { LoadingButton } from '@mui/lab';
+import { useSnackbar } from 'notistack';
 
 export default function ContactForm(): JSX.Element {
-  const theme = useTheme();
   const isSmallScreen = useMediaQuery("(max-width:1280px)");
   const { enqueueSnackbar } = useSnackbar();
 
-  const [formData, setFormData] = useState({
-    name: "",
-    subject: "",
-    email: "",
-    phone: "",
-    message: "",
-  });
-
-  const [isSending, setIsSending] = useState(false);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
+  const initialValues = {
+    name: '',
+    subject: '',
+    email: '',
+    phone: '',
+    message: '',
   };
 
-  // const sendEmail = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   setIsSending(true);
+  
 
-  //   try {
-  //     const res = await emailjs.send(
-  //       "service_qzgq94s", // Replace with your EmailJS service ID
-  //       "template_t3djn0f", // Replace with your EmailJS template ID
-  //       {
-  //         firstName: formData.firstName,
-  //         lastName: formData.lastName,
-  //         email: formData.email,
-  //         phone: formData.phone,
-  //         message: formData.message,
-  //       },
-  //       "th0A-KVSYM9GlnJTc" // Replace with your EmailJS user ID
-  //     );
-
-  //     console.log("RES", res);
-
-  //     enqueueSnackbar("Message sent successfully,Thank You!", {
-  //       variant: "success",
-  //       autoHideDuration: 3000,
-  //     });
-  //     setFormData({
-  //       firstName: "",
-  //       lastName: "",
-  //       email: "",
-  //       phone: "",
-  //       message: "",
-  //     });
-  //   } catch (error) {
-  //     enqueueSnackbar("Error while sending the message.", {
-  //       variant: "error",
-  //       autoHideDuration: 3000,
-  //     });
-  //     console.error("EmailJS error:", error);
-  //   } finally {
-  //     setIsSending(false);
-  //   }
-  // };
-
-  const sendEmail = async(e: React.FormEvent)=>{
-    e.preventDefault();
+  const sendEmail = async(values:{name:string,subject:string,email:string,phone:string,message:string},{resetForm}:any)=>{
     try{
-      setIsSending(true);
       const res = await fetch('/api/contactForm',{
         method:'POST',
         headers: {
           'Content-Type': 'application/json',
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(values),
       })
       if(res.ok){
       enqueueSnackbar("Message sent successfully,Thank You!", {
@@ -107,180 +48,206 @@ export default function ContactForm(): JSX.Element {
             });
     }
     finally{
-      setIsSending(false)
-           setFormData({
-              name: "",
-              subject: "",
-              email: "",
-              phone: "",
-              message: "",
-            });
+      resetForm();
     }
-   
-
-    
   }
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    subject: Yup.string().required('Subject is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    phone: Yup.string().required('Phone number is required'),
+    message: Yup.string().required('Message is required'),
+  });
+
   return (
     <Box sx={{ padding: "60px" }}>
-      <form onSubmit={sendEmail}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            flexWrap: "wrap",
-            [theme.breakpoints.down(800)]: {
-              textAlign: "center",
-            },
-            mb: 6,
-          }}
-        >
-          <TextField
-            id="name"
-            label="Name"
-            variant="standard"
-            fullWidth={isSmallScreen}
-            value={formData.name}
-            onChange={handleInputChange}
-            required
-            sx={{
-              mb: isSmallScreen ? 2 : 0,
-              "& .MuiInputLabel-root": { color: "black" },
-              "& .MuiInputLabel-root.Mui-focused": { color: "black" },
-              "& .MuiInput-underline:before": { borderBottomColor: "black" },
-              "& .MuiInput-underline:hover:before": {
-                borderBottomColor: "black",
-              },
-              "& .MuiInput-underline:after": { borderBottomColor: "black" },
-            }}
-          />
-          <TextField
-            id="subject"
-            label="Subject"
-            variant="standard"
-            fullWidth={isSmallScreen}
-            value={formData.subject}
-            onChange={handleInputChange}
-            required
-            sx={{
-              "& .MuiInputLabel-root": { color: "black" },
-              "& .MuiInputLabel-root.Mui-focused": { color: "black" },
-              "& .MuiInput-underline:before": { borderBottomColor: "black" },
-              "& .MuiInput-underline:hover:before": {
-                borderBottomColor: "black",
-              },
-              "& .MuiInput-underline:after": { borderBottomColor: "black" },
-            }}
-          />
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            flexWrap: "wrap",
-            [theme.breakpoints.down(800)]: {
-              textAlign: "center",
-            },
-            mb: 4,
-          }}
-        >
-          <TextField
-            id="email"
-            label="Email"
-            variant="standard"
-            fullWidth={isSmallScreen}
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-            sx={{
-              mb: isSmallScreen ? 2 : 0,
-              "& .MuiInputLabel-root": { color: "black" },
-              "& .MuiInputLabel-root.Mui-focused": { color: "black" },
-              "& .MuiInput-underline:before": { borderBottomColor: "black" },
-              "& .MuiInput-underline:hover:before": {
-                borderBottomColor: "black",
-              },
-              "& .MuiInput-underline:after": { borderBottomColor: "black" },
-            }}
-          />
-          <TextField
-            id="phone"
-            label="Phone"
-            variant="standard"
-            fullWidth={isSmallScreen}
-            value={formData.phone}
-            onChange={handleInputChange}
-            required
-            sx={{
-              "& .MuiInputLabel-root": { color: "black" },
-              "& .MuiInputLabel-root.Mui-focused": { color: "black" },
-              "& .MuiInput-underline:before": { borderBottomColor: "black" },
-              "& .MuiInput-underline:hover:before": {
-                borderBottomColor: "black",
-              },
-              "& .MuiInput-underline:after": { borderBottomColor: "black" },
-            }}
-          />
-        </Box>
-
-        <TextField
-          id="message"
-          label="Message"
-          placeholder="Write your message..."
-          variant="standard"
-          fullWidth
-          multiline
-          rows={2}
-          value={formData.message}
-          onChange={handleInputChange}
-          required
-          sx={{
-            mb: 2,
-            "& .MuiInputLabel-root": { color: "black" },
-            "& .MuiInputLabel-root.Mui-focused": { color: "black" },
-            "& .MuiInput-underline:before": { borderBottomColor: "black" },
-            "& .MuiInput-underline:hover:before": {
-              borderBottomColor: "black",
-            },
-            "& .MuiInput-underline:after": { borderBottomColor: "black" },
-          }}
-        />
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-         <LoadingButton
-            type="submit"
-            loading={isSending}
-            variant="contained"
-            loadingPosition="start"
-            fullWidth
-            sx={{
-              backgroundColor: isSending ? "#666" : "black",
-              color: "white",
-              mt: 4,
-              paddingY: "12px",
-              fontSize: "14px",
-              minWidth: "auto",
-              "&.Mui-disabled": {
-                backgroundColor: "#666", // Style for disabled state
-              },
-              "&:hover": {
-                backgroundColor: isSending ? "#666" : "#333", // Disable hover effect if loading
-              },
-            }}
-            disabled={isSending}
-          >
-            {isSending ? "Sending..." : "Send Message"}
-          </LoadingButton>
-        </Box>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={sendEmail}
+      >
+        {({ isSubmitting,errors }) => {
+          const errorsLength = Object.keys(errors).length>0;
+          return (
+            <Form>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  mb: 6,
+                }}
+              >
+                <Field name="name">
+                  {({ field }: { field: any }) => (
+                    <TextField
+                      {...field}
+                      id="name"
+                      label="Name"
+                      variant="standard"
+                      fullWidth={isSmallScreen || errorsLength}
+                      sx={{
+                        mb:1,
+                        "& .MuiInputLabel-root": { color: "black" },
+                        "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+                        "& .MuiInput-underline:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:hover:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:after": { borderBottomColor: "black" },
+                      }}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="name">
+                  {(msg) => <Typography color="error">{msg}</Typography>}
+                </ErrorMessage>
+  
+                <Field name="subject">
+                  {({ field }: { field: any }) => (
+                    <TextField
+                      {...field}
+                      id="subject"
+                      label="Subject"
+                      variant="standard"
+                      fullWidth={isSmallScreen || errorsLength}
+                      sx={{
+                        mb:1,
+                        "& .MuiInputLabel-root": { color: "black" },
+                        "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+                        "& .MuiInput-underline:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:hover:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:after": { borderBottomColor: "black" },
+                      }}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="subject">
+                  {(msg) => <Typography color="error">{msg}</Typography>}
+                </ErrorMessage>
+              </Box>
+  
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  mb: 4,
+                }}
+              >
+                <Field name="email">
+                  {({ field }: { field: any }) => (
+                    <TextField
+                      {...field}
+                      id="email"
+                      label="Email"
+                      variant="standard"
+                      fullWidth={isSmallScreen || errorsLength}
+                      sx={{
+                        mb:1,
+                        "& .MuiInputLabel-root": { color: "black" },
+                        "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+                        "& .MuiInput-underline:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:hover:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:after": { borderBottomColor: "black" },
+                      }}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="email">
+                  {(msg) => <Typography color="error">{msg}</Typography>}
+                </ErrorMessage>
+  
+                <Field name="phone">
+                  {({ field }: { field: any }) => (
+                    <TextField
+                      {...field}
+                      id="phone"
+                      label="Phone"
+                      variant="standard"
+                      fullWidth=  {isSmallScreen || errorsLength}
+                      sx={{
+                        mb:1,
+                        "& .MuiInputLabel-root": { color: "black" },
+                        "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+                        "& .MuiInput-underline:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:hover:before": { borderBottomColor: "black" },
+                        "& .MuiInput-underline:after": { borderBottomColor: "black" },
+                      }}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="phone">
+                  {(msg) => <Typography color="error">{msg}</Typography>}
+                </ErrorMessage>
+              </Box>
+  
+              <Field name="message">
+                {({ field }: { field: any }) => (
+                  <TextField
+                    {...field}
+                    id="message"
+                    label="Message"
+                    placeholder="Write your message..."
+                    variant="standard"
+                    fullWidth
+                    multiline
+                    rows={2}
+                    sx={{
+                      "& .MuiInputLabel-root": { color: "black" },
+                      "& .MuiInputLabel-root.Mui-focused": { color: "black" },
+                      "& .MuiInput-underline:before": { borderBottomColor: "black" },
+                      "& .MuiInput-underline:hover:before": {
+                        borderBottomColor: "black",
+                      },
+                      "& .MuiInput-underline:after": {
+                        borderBottomColor: "black",
+                      },
+                    }}
+                  />
+                )}
+              </Field>
+              <ErrorMessage name="message">
+                {(msg) => <Typography color="error">{msg}</Typography>}
+              </ErrorMessage>
+  
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+               <LoadingButton
+                  type="submit"
+                  loading={isSubmitting}
+                  variant="contained"
+                  loadingPosition="start"
+                  fullWidth
+                  sx={{
+                    backgroundColor:isSubmitting ? "#666" : "black",
+                    color:"white",
+                    mt:4,
+                    paddingY:"12px",
+                    fontSize:"14px",
+                    minWidth:"auto",
+                    "&.Mui-disabled": {
+                       backgroundColor:"#666", // Style for disabled state
+                     },
+                     "&.hover":{
+                       backgroundColor:isSubmitting ? "#666":"#333", // Disable hover effect if loading 
+                     }
+                   }}
+                   disabled={isSubmitting}
+                 >
+                   {isSubmitting ? 'Sending...' : 'Send Message'}
+                 </LoadingButton>
+              </Box>
+            </Form>
+          )
+        }}
+      </Formik>
     </Box>
   );
 }
